e86924853ee6e6f045b368f297625ce88237df5e
Termi <escarpaprieto@gmail.com>
2021-08-03 17:57:00 +0200

    User input

--- AnimeTimeCalculator/Program.cs
+++ AnimeTimeCalculator/Program.cs
@@ -10,6 +10,44 @@ namespace AnimeTimeCalculator
     {
         static void Main(string[] args)
         {
+            int cap;
+            int dias;
+            int horas;
+            int minutos;
+            cap = capInput();
+
+        }
+
+        static int capInput()
+        {
+            bool validInput = false;
+            int userCapInput = 0 ;
+            do
+            {
+                Console.WriteLine("El calculo se realizará teniendo en cuenta que cada capítulo");
+                Console.WriteLine("dura una aproximación de 25 min.");
+                Console.WriteLine("Introduzca el numero total de capítulos del anime: ");
+                try
+                {
+                    userCapInput = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("\n---------------------------");
+                    Console.WriteLine("Valor introducido no valido");
+                    Console.WriteLine("---------------------------\n");
+                    //Console.WriteLine(e);
+                }
+
+                if (userCapInput != 0)
+                {
+                    validInput = true;
+                }
+
+            }
+            while (!validInput);
+
+            return userCapInput;
         }
     }
 }
